883a15f8200d9370b0b3ba08fb559da3e4018c22
Dasgupta <gaurab.dasgupta@tesco.com>
2023-07-12 18:09:34 +0530

    cleanup

--- tesco-spring-core2/TescoJavaTrainingDay26-tesco-spring-jdbc/src/main/java/com/dao/EmployeeDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dao;
-
-import com.model.Employee;
-
-public interface EmployeeDAO {
-	public void insertEmployee(Employee e);
-
-}
--- tesco-spring-core2/TescoJavaTrainingDay26-tesco-spring-jdbc/src/main/java/com/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import org.springframework.stereotype.Component;
-
-import com.model.Employee;
-
-public interface EmployeeService {
-	public void insertEmployee(Employee e);
-}
